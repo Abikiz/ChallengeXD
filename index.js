dca10b3f370f8ba68efb3cb36ea36419fda8265a
AbigailLopez <abigail.malvaez06@gmail.com>
2023-01-05 05:04:20 -0600

    ultimo commit

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//sql connection
-const sql = require("msnodesqlv8");
-
-
 //for te static documents
 app.use(express.static(__dirname + "/public"));
 
